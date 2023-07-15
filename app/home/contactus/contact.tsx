ba655b1790a1d4f1fbbb849ade059e536c7172be
Devanshu <devanshuv1501@gmail.com>
2023-07-16 04:56:02 +0530

    Fixed build issue

--- app/home/contactus/contact.tsx
+++ app/home/contactus/contact.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <div className="grig gri-cols-12 py-10 max-w-4xl mx-auto px-3 md:px-0">
             <div className="col-span-12">
                 <h1 className="text-4xl text-center">Not Able to Connect With Us?</h1>
-                <h2 className="text-green-700 text-center">No Worries we'll get in touch with you soon.</h2>
+                <h2 className="text-green-700 text-center">No Worries we will get in touch with you soon.</h2>
                 <Contactform/>
             </div>
         </div>
